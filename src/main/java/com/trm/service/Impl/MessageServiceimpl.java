ab8eebccdef2eed75e895e023f154cfdd8ea7764
zfj111 <qq15932061953@163.com>
2019-03-26 11:43:29 +0800

    zfj1

--- src/main/java/com/trm/service/Impl/MessageServiceimpl.java
+++ src/main/java/com/trm/service/Impl/MessageServiceimpl.java
@@ -3,9 +3,10 @@ package com.trm.service.Impl;
 import com.trm.models.Message;
 import com.trm.repositroy.MessageMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class MessageServiceimpl implements MessageMapper {
     @Autowired(required = false)
     private MessageMapper mapper;
