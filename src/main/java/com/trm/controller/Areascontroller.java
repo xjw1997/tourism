d4d80b8b266140f7a260fea1917e4070700c32e4
a6229708 <759416923@qq.com>
2019-03-26 11:36:10 +0800

    111

--- src/main/java/com/trm/controller/Areascontroller.java
+++ src/main/java/com/trm/controller/Areascontroller.java
@@ -2,8 +2,10 @@ package com.trm.controller;
 
 import com.trm.models.Areas;
 import com.trm.service.AreasService;
+import com.trm.service.Impl.AreasServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 public class Areascontroller {
     @Autowired(required = false)
     private AreasService areasService;
+    @RequestMapping("/areaslist")
     public ModelAndView selectall(ModelAndView mv){
         List<Areas> list = areasService.getAll();
         mv.addObject("areaslist",list);
