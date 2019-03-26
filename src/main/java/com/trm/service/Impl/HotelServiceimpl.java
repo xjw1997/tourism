76d828874d27f20b553a18f83e71e95c813be4b7
xjw1997 <1070922300@qq.com>
2019-03-26 11:45:16 +0800

    框架

--- src/main/java/com/trm/service/Impl/HotelServiceimpl.java
+++ src/main/java/com/trm/service/Impl/HotelServiceimpl.java
@@ -4,9 +4,10 @@ import com.trm.models.Hotel;
 import com.trm.repositroy.HotelMapper;
 import com.trm.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class HotelServiceimpl implements HotelService {
     @Autowired(required = false)
     private HotelMapper mapper;
