c37610d80157665fc3a3198a02fe5b1cd370c097
y19990329 <2970173155@qq.com   43031660+y19990329@users.noreply.github.com>
2019-03-27 15:27:42 +0800

    jqy

--- src/main/java/com/trm/controller/Admincontroller.java
+++ src/main/java/com/trm/controller/Admincontroller.java
@@ -4,6 +4,7 @@ import com.trm.models.Admin;
 import com.trm.service.Impl.AdminServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,12 +12,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class Admincontroller {
     @Autowired(required = false)
     private AdminServiceimpl adminServiceimpl;
+    @RequestMapping("/lg")
+    public  String  lg(){
+        return "login";
+    }
 
-    public ModelAndView login(ModelAndView mv , @RequestParam String username ,@RequestParam String password){
+    @RequestMapping("/enter")
+    public ModelAndView login(ModelAndView mv , @RequestParam("username") String username ,@RequestParam("password") String password){
         Admin admin = adminServiceimpl.login(username, password);
           if (admin.getUsername() != null){
               mv.addObject("admin",admin);
-              mv.setViewName("/home");
+              mv.setViewName("redirect:/areaslist");
               return mv;
           }else{
               mv.setViewName("/login");
