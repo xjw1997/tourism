edc3e459754cb765ace37d09d89b148b93978cd0
xjw1997 <1070922300@qq.com>
2019-03-27 11:24:35 +0800

    框架

--- src/main/java/com/trm/controller/Admincontroller.java
+++ src/main/java/com/trm/controller/Admincontroller.java
@@ -3,6 +3,7 @@ package com.trm.controller;
 import com.trm.service.Impl.AdminServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,6 +11,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class Admincontroller {
     @Autowired(required = false)
     private AdminServiceimpl adminServiceimpl;
+
+    @RequestMapping("/dl")
+    public String dl(){
+        return "/login";
+    }
+
     public ModelAndView login(ModelAndView mv , @RequestParam String adname ,@RequestParam String password){
         return  mv;
     }
